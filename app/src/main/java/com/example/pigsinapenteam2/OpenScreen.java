5af5b3c4c6f02a2b0bc3c55cef66946f4e256901
osc-jaimes <oscjaims@gmail.com>
2019-11-01 19:29:27 -0700

    Added the base for the opening screen, XML and Java Class

---
+++ app/src/main/java/com/example/pigsinapenteam2/OpenScreen.java
@@ -0,0 +1,14 @@
+package com.example.pigsinapenteam2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenScreen extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_open_screen);
+  }
+}
