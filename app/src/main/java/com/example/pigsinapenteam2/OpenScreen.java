23a2a17f3e3da2369f64935cf355ded97057cb47
osc-jaimes <oscjaims@gmail.com>
2019-11-07 13:38:03 -0800

    finished the opening screen and added button functionality to go to the main screen of the game

--- app/src/main/java/com/example/pigsinapenteam2/OpenScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/OpenScreen.java
@@ -2,7 +2,9 @@ package com.example.pigsinapenteam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OpenScreen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class OpenScreen extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_open_screen);
+  }
+
+  public void pressStartButton(View V){
+    Intent goToMainScreen = new Intent(getApplicationContext(), MainScreen.class);
+    startActivity(goToMainScreen);
   }
 }
