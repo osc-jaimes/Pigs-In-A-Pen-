70a42f636e7725b04f6e68b1564dd561a773ada8
osc-jaimes <oscjaims@gmail.com>
2019-11-12 11:32:21 -0800

    started foundation for multiplayer and singleplayer choices on the main screen

--- app/src/main/java/com/example/pigsinapenteam2/MainScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/MainScreen.java
@@ -2,7 +2,9 @@ package com.example.pigsinapenteam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainScreen extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_screen);
+  }
+
+  public void pressSinglePlayerButton(View v){
+    Intent goToSinglePlayerScreen = new Intent(getApplicationContext(), SinglePlayerScreen.class);
+    startActivity(goToSinglePlayerScreen);
+  }
+
+  public void pressMultiPlayerButton(View v){
+
   }
 }
