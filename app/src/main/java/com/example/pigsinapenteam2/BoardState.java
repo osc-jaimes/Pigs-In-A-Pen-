528228f1320fbfea31e71ac60a7eec27f1b89cd1
Benjamin <bwilson1@ualberta.ca>
2019-11-11 20:46:24 -0700

    added default constructor & class variables

--- app/src/main/java/com/example/pigsinapenteam2/BoardState.java
+++ app/src/main/java/com/example/pigsinapenteam2/BoardState.java
@@ -2,6 +2,23 @@ package com.example.pigsinapenteam2;
 
 /**
  * stores the state of the board and all inputted data.
+ * The board is a 3D array with a format as follows:
+ * [x coordinate][y coordinate][wall data and cell data]
  */
 public class BoardState {
+
+  private int[][][] boardData;
+  private int width;
+  private int height;
+
+  /**
+   * default constructor for BoardState. Sets the board as a 10x10 empty board
+   *
+   */
+  public BoardState() {
+
+    boardData = new int[10][10][5];
+    width = 10;
+    height = 10;
+  }
 }
