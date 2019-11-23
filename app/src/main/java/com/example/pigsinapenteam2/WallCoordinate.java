1e1e8cb4cd0f8a16de23e2f75510243daf3eee68
Luke Rostad <rostad@ualberta.ca>
2019-11-23 14:05:17 -0700

    added extra constructors, isVertical, isHorizontal, and some documentation

--- app/src/main/java/com/example/pigsinapenteam2/WallCoordinate.java
+++ app/src/main/java/com/example/pigsinapenteam2/WallCoordinate.java
@@ -1,13 +1,52 @@
 package com.example.pigsinapenteam2;
 
+/**
+ * WallCoordinate: for storing coordinates easily.
+ *
+ *
+ */
 public class WallCoordinate {
   public int x;
   public int y;
   private int wallPosition; //0 to 3
 
+  /**
+   * WallCoordinates: Constructor
+   * @param xCoord
+   * @param yCoord
+   * @param wallPos: 0 for top, 1 for right, onwards clockwise (max 3)
+   */
   public void WallCoordinate(int xCoord, int yCoord, int wallPos) {
     x = xCoord;
     y = yCoord;
     wallPosition = wallPos;
+  }
+
+  /**
+   * default constructor
+   */
+  public void WallCoordinate() {
+    x = 0;
+    y = 0;
+    wallPosition = 0;
+  }
+
+  /**
+   * incomplete constructor: sets wallPosition to top (0)
+   * @param xCoord
+   * @param yCoord
+   */
+  public void wallCoordinate(int xCoord, int yCoord) {
+    x = xCoord;
+    y = yCoord;
+    wallPosition = 0;
+  }
+
+  public boolean isVertical() {
+    return (wallPosition % 2 == 1);
+  }
+
+  public boolean isHorizontal() {
+    return (wallPosition % 2 == 0);
   }
 }
