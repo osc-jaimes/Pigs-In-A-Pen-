4e53baafa08f563215d8ab008bb39bafededaffd
Luke Rostad <rostad@ualberta.ca>
2019-11-23 13:31:57 -0700

    did basic structure of EasyBotPlayer

--- app/src/main/java/com/example/pigsinapenteam2/EasyBotPlayer.java
+++ app/src/main/java/com/example/pigsinapenteam2/EasyBotPlayer.java
@@ -1,5 +1,24 @@
 package com.example.pigsinapenteam2;
 
-public class EasyBotPlayer extends BotPlayer {
+import GameState;
 
+/**
+ * EasyBotPlayer: (easy mode) plays so you don't have to.
+ */
+public class EasyBotPlayer extends BotPlayer {
+  private int boardHeight;
+  private int boardWidth;
+
+  private int[][][][] possibleMoves;
+  private int[][][][] possibleCaptures;
+
+  public void EasyBotPlayer(int height, int width) {
+    boardHeight = height;
+    boardWidth = width;
+  }
+
+  @Override
+  public GameState doMove(GameState inputState) {
+    return super.doMove(inputState);
+  }
 }
