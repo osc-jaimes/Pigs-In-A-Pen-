3332b46df6864c681acd1359bed219c6aa8dc574
osc-jaimes <oscjaims@gmail.com>
2019-11-23 12:11:00 -0800

    added button functionality to single player setup screen in order to start a game, working on button placement and aarangment next for single player play screen

--- app/src/main/java/com/example/pigsinapenteam2/SinglePlayerScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/SinglePlayerScreen.java
@@ -2,7 +2,9 @@ package com.example.pigsinapenteam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SinglePlayerScreen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SinglePlayerScreen extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_single_player_screen);
+  }
+
+  public void pressStartGameButton(View V){
+    Intent goToSinglePlayerPlayScreen = new Intent(getApplicationContext(), SinglePlayerPlayScreen.class);
+    startActivity(goToSinglePlayerPlayScreen);
   }
 }
