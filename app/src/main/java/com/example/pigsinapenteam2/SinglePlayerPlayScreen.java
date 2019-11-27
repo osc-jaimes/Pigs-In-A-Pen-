575586dbed5621ceb0d1d2d684ee229553deb084
Jared <jarrrboon@gmail.com>
2019-11-27 14:50:31 -0700

    Changed onclick methods to be better

--- app/src/main/java/com/example/pigsinapenteam2/SinglePlayerPlayScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/SinglePlayerPlayScreen.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 
 public class SinglePlayerPlayScreen extends AppCompatActivity {
-//Instance Variables + instantiations
+  //Instance Variables + instantiations
   GameState gameState;
   HumanPlayer player1;
   View confirmButton = findViewById(R.id.confirmButton);
+  boolean confirmButtonPressed;
   //
 
   @Override
@@ -19,71 +20,153 @@ public class SinglePlayerPlayScreen extends AppCompatActivity {
     //setting confirm button to not exist for now
     confirmButton.setVisibility(View.GONE);
   }
-
-  /**
-   * onClick
-   *
-   * On user click, the button will first light up, and on click of confirm button the set move will pass on
-   * @param v The view on where the buttons reside
-   */
-  public void onClick(View v) {
+  public void onClickHorizontal1(View v) {
     if (!player1.getHasMoved()) {
       confirmButton.setVisibility(View.VISIBLE);
-      if (v.findViewById(R.id.confirmButton)){
-        switch (v.findViewById()) {
-          case (R.id.H1):
-            player1.doMove(gameState, 0, 0, true);
-            break;
-          case (R.id.H2):
-            player1.doMove(gameState, 0, 1, true);
-            break;
-          case (R.id.H3):
-            player1.doMove(gameState, 0, 2, true);
-            break;
-          case (R.id.H4):
-            player1.doMove(gameState, 0, 3, true);
-            break;
-          case (R.id.H5):
-            player1.doMove(gameState, 1, 0, true);
-            break;
-          case (R.id.H6):
-            player1.doMove(gameState, 1, 1, true);
-            break;
-          case (R.id.H7):
-            player1.doMove(gameState, 1, 2, true);
-            break;
-          case (R.id.H8):
-            player1.doMove(gameState, 1, 3, true);
-            break;
-          case (R.id.H9):
-            player1.doMove(gameState, 1, 4, true);
-            break;
-          case (R.id.V1):
-            player1.doMove(gameState, 0, 0, false);
-            break;
-          case (R.id.V2):
-            player1.doMove(gameState, 1, 0, false);
-            break;
-          case (R.id.V3):
-            player1.doMove(gameState, 2, 0, false);
-            break;
-          case (R.id.V4):
-            player1.doMove(gameState, 3, 0, false);
-            break;
-          case (R.id.V5):
-            player1.doMove(gameState, 0, 1, false);
-            break;
-          case (R.id.V6):
-            player1.doMove(gameState, 1, 1, false);
-            break;
-          case (R.id.V7):
-            player1.doMove(gameState, 2, 1, false);
-            break;
-          case (R.id.V8):
-            player1.doMove(gameState, 3, 1, false);
-            break;
-        }
-      }
+      confirmButtonPressed = false;
     }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 0, 0, true);
+    }
+  }
+  public void onClickHorizontal2(View v) {
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 0, 1, true);
+    }
+  }
+  public void onClickHorizontal3(View v) {
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 0, 2, true);
+    }
+  }
+  public void onClickHorizontal14(View v) {
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 0, 3, true);
+    }
+  }
+  public void onClickHorizontal5(View v) {
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 1, 0, true);
+    }
+  }
+  public void onClickHorizontal16(View v) {
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 1, 1, true);
+    }
+  }
+  public void onClickHorizontal7(View v) {
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 1, 2, true);
+    }
+  }
+  public void onClickHorizontal8(View v) {
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 1, 3, true);
+    }
+  }
+  public void onClickHorizontal9(View v) {
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 0, 0, true);
+    }
+  }
+  public void onClickVertical1(View v){
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 1, 0, false);
+    }
+  }
+  public void onClickVertical2(View v){
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 2, 0, false);
+    }
+  }
+  public void onClickVertical3(View v){
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 3, 0, false);
+    }
+  }
+  public void onClickVertical4(View v){
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 0, 1, false);
+    }
+  }
+  public void onClickVertical5(View v){
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 1, 1, false);
+    }
+  }
+  public void onClickVertical6(View v){
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 2, 1, false);
+    }
+  }
+  public void onClickVertical8(View v){
+    if (!player1.getHasMoved()) {
+      confirmButton.setVisibility(View.VISIBLE);
+      confirmButtonPressed = false;
+    }
+    if(confirmButtonPressed = true){
+      player1.doMove(gameState, 3, 1, false);
+    }
+  }
+
+  public void onClickConfirmationButton(View v) {
+    confirmButtonPressed = true;
+    onClickHorizontal1(v);
   }
 }
