83261e2f9a3048370e54b7bbfd2dc1fcb32d8297
osc-jaimes <oscjaims@gmail.com>
2019-11-27 11:00:08 -0800

    working on buttons on playboard. Got the invisible buttons to work on click, have to connect to backend now. Will do this later today, November 27/2019

--- app/src/main/java/com/example/pigsinapenteam2/SinglePlayerPlayScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/SinglePlayerPlayScreen.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SinglePlayerPlayScreen extends AppCompatActivity {
 
@@ -15,6 +18,16 @@ public class SinglePlayerPlayScreen extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_single_player_play_screen);
   }
+
+  public void buttonClicked(View V){
+    int buttonId = V.getId();
+    Button theButton = findViewById(buttonId);
+    theButton.setBackgroundColor(getResources().getColor(R.color.buttonColor));
+
+  }
+
+
+
 
 
 
