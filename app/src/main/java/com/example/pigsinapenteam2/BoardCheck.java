93f8548f9f9cca9f79e50204df9feec846245dd0
Benjamin <bwilson1@ualberta.ca>
2019-11-27 15:42:29 -0700

    added constructor; added boardCheck

--- app/src/main/java/com/example/pigsinapenteam2/BoardCheck.java
+++ app/src/main/java/com/example/pigsinapenteam2/BoardCheck.java
@@ -3,6 +3,31 @@ package com.example.pigsinapenteam2;
 
 public class BoardCheck {
 
+  int playerOneScore;
+  int playerTwoScore;
+  BoardState currentBoard;
+
+  public BoardCheck( BoardState instanceBoard){
+
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    currentBoard = instanceBoard;
+
+  }//BoardCheckConstructor
+
+  public void boardChecker(){
+
+    for(int i = 0; i <= currentBoard.getWidth(); i++){
+      for(int j = 0; j <= currentBoard.getHeight(); j++){
+
+        cellScoreCheck(i,j);
+
+      }//inner for loop
+    }//for loop
+  }//boardChecker
+
+  private void cellScoreCheck(int xCoord, int yCoord)
+
 
 
 }
