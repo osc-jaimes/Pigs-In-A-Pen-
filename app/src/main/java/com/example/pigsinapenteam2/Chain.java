bcaa90657f74959eb25435693e8de15d0aec71f0
Luke Rostad <rostad@ualberta.ca>
2019-11-27 17:13:15 -0700

    added data fields to Chain: length, head, tail, and some booleans. Also the default constructor.

--- app/src/main/java/com/example/pigsinapenteam2/Chain.java
+++ app/src/main/java/com/example/pigsinapenteam2/Chain.java
@@ -1,4 +1,19 @@
 package com.example.pigsinapenteam2;
 
 public class Chain {
+  int length;
+  WallCoordinate head;
+  WallCoordinate tail;
+  boolean isHeadOpen;
+  boolean isTailOpen;
+  //a tail or head would be "closed" if you can place a wall there
+  //   and immediately get a point.
+
+  public void Chain() {
+    length = 0;
+    head = new WallCoordinate();
+    tail = new WallCoordinate();
+    isHeadOpen = true;
+    isTailOpen = true;
+  }
 }
