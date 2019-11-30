ba718348d6bd72d4424eff880a0dc7eec80b2853
osc-jaimes <oscjaims@gmail.com>
2019-11-30 10:45:24 -0800

    got the confirm button to work as well as changing move before hitting confirm button

--- app/src/main/java/com/example/pigsinapenteam2/SinglePlayerPlayScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/SinglePlayerPlayScreen.java
@@ -9,9 +9,12 @@ import android.widget.Button;
 public class SinglePlayerPlayScreen extends AppCompatActivity {
   //Instance Variables + instantiations
   GameState gameState;
+  HumanPlayer player1;
+  BotPlayer botPlayer;
   Button confirmButton;
   int cellX;
   int cellY;
+  boolean playerHasMoved;
   boolean isHorizontal;
   Button currentButton;
   boolean confirmedAction;
@@ -25,10 +28,10 @@ public class SinglePlayerPlayScreen extends AppCompatActivity {
     setContentView(R.layout.activity_single_player_play_screen);
     this.confirmButton = findViewById(R.id.confirmButtonPlayer1);
     //setting confirm button to not exist for now
-   confirmButton.setVisibility(View.GONE);
-   this.currentPlayer = player1;
-   this.playerHasMoved = false;
-   this.currentButton = null;
+    confirmButton.setVisibility(View.GONE);
+    this.currentPlayer = player1;
+    this.playerHasMoved = false;
+    this.currentButton = null;
   }
 
   public void buttonClicked(View V){
@@ -36,6 +39,7 @@ public class SinglePlayerPlayScreen extends AppCompatActivity {
       int buttonId = V.getId();
       Button currentButton = findViewById(buttonId);
       this.currentButton = currentButton;
+      this.confirmButton.setVisibility(View.VISIBLE);
       currentButton.setBackgroundColor(getResources().getColor(R.color.fences));
     } else{
       this.changeChoice(V);
@@ -49,236 +53,165 @@ public class SinglePlayerPlayScreen extends AppCompatActivity {
     Button buttonClicked = findViewById(buttonId);
     this.currentButton.setBackgroundColor(getResources().getColor(R.color.transparent));
     buttonClicked.setBackgroundColor(getResources().getColor(R.color.fences));
+    this.currentButton = buttonClicked;
   }
 
   public void onClickVertical1(View v){
-    if(!playerHasMoved) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
-
+    buttonClicked(v);
     cellX = 0;
     cellY = 0;
     isHorizontal = false;
   }
 
   public void onClickVertical2(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      this.buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 1;
     cellY = 0;
     isHorizontal = false;
   }
 
   public void onClickVertical3(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 2;
     cellY = 0;
     isHorizontal = false;
   }
 
   public void onClickVertical4(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 0;
     cellY = 1;
     isHorizontal = false;
   }
 
   public void onClickVertical5(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 1;
     cellY = 1;
     isHorizontal = false;
   }
 
   public void onClickVertical6(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 2;
     cellY = 1;
     isHorizontal = false;
   }
 
   public void onClickVertical7(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 0;
     cellY = 2;
     isHorizontal = false;
   }
 
   public void onClickVertical8(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 1;
     cellY = 2;
     isHorizontal = false;
   }
   public void onClickVertical9(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 2;
     cellY = 2;
     isHorizontal = false;
   }
   public void onClickVertical10(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 0;
     cellY = 3;
     isHorizontal = false;
   }
   public void onClickVertical11(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 1;
     cellY = 3;
     isHorizontal = false;
   }
   public void onClickVertical12(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 2;
     cellY = 3;
     isHorizontal = false;
   }
   public void onClickHorizontal1(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 0;
-    cellY = 3;
+    cellY = 0;
     isHorizontal = true;
   }
   public void onClickHorizontal2(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 1;
     cellY = 0;
     isHorizontal = true;
   }
   public void onClickHorizontal3(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 2;
     cellY = 0;
     isHorizontal = true;
   }
   public void onClickHorizontal4(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 3;
     cellY = 0;
     isHorizontal = true;
   }
   public void onClickHorizontal5(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 0;
     cellY = 1;
     isHorizontal = true;
   }
   public void onClickHorizontal6(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 1;
     cellY = 1;
     isHorizontal = true;
   }
   public void onClickHorizontal7(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 2;
     cellY = 1;
     isHorizontal = true;
   }
   public void onClickHorizontal8(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 3;
     cellY = 1;
     isHorizontal = true;
   }
   public void onClickHorizontal9(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 0;
     cellY = 2;
     isHorizontal = true;
   }
   public void onClickHorizontal10(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 1;
     cellY = 2;
     isHorizontal = true;
   }
   public void onClickHorizontal11(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 2;
     cellY = 2;
     isHorizontal = true;
   }
   public void onClickHorizontal12(View v) {
-    if (!player1.getHasMoved()) {
-      confirmButton.setVisibility(View.VISIBLE);
-      buttonClicked(v);
-    }
+    this.buttonClicked(v);
     cellX = 3;
     cellY = 2;
     isHorizontal = true;
   }
 
   public void onClickConfirmationButton(View v) {
-    //confirmAction(cellX, cellY, isHorizontal);
-    //confirmButton.setVisibility(View.GONE);
+    this.currentButton = null;
+    this.playerHasMoved = true;
+    this.confirmButton.setVisibility(View.GONE);
   }
 
   public void confirmAction(int cellX, int cellY, boolean isHorizontal){
