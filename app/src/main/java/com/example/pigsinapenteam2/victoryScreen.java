700332cb773b46d87d1a42066edd3ce3df87141f
Jared <jarrrboon@gmail.com>
2019-12-01 16:03:12 -0700

    made victory screen

--- app/src/main/java/com/example/pigsinapenteam2/victoryScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/victoryScreen.java
@@ -2,7 +2,9 @@ package com.example.pigsinapenteam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class victoryScreen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class victoryScreen extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_victory_screen);
+  }
+
+  public void menuButton(View v){
+    Intent goToMainMenu = new Intent(getApplicationContext(), MainScreen.class);
+    startActivity(goToMainMenu);
+  }
+  public void restartButton(View v){
+    Intent goToGameStart = new Intent(getApplicationContext(), SinglePlayerPlayScreen.class);
+    startActivity(goToGameStart);
   }
 }
