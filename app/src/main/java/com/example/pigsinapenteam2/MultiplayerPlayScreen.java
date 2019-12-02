481252df1000fd7d4bf47dbe5763452efaed582a
osc-jaimes <oscjaims@gmail.com>
2019-12-01 20:43:11 -0800

    Added XML layout for multiplayer play screen as well as button functionality for moving from setup screen to play screen

---
+++ app/src/main/java/com/example/pigsinapenteam2/MultiplayerPlayScreen.java
@@ -0,0 +1,20 @@
+package com.example.pigsinapenteam2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiplayerPlayScreen extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_multiplayer_play_screen);
+  }
+
+
+
+  
+
+
+}
--- app/src/main/java/com/example/pigsinapenteam2/MultiplayerSetupScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/MultiplayerSetupScreen.java
@@ -2,7 +2,9 @@ package com.example.pigsinapenteam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MultiplayerSetupScreen extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MultiplayerSetupScreen extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_multiplayer_setup_screen);
+  }
+
+  /**
+   * Starts the game with the respective choices made in the setup screen.
+   * @param V the button to press to start the game.
+   */
+  public void playGame(View V){
+    Intent goToPlayScreen = new Intent(getApplicationContext(), MultiplayerPlayScreen.class);
+    startActivity(goToPlayScreen);
   }
 }
