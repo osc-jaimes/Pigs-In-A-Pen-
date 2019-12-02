5e8c2e3aa8f6e18d257d27d4010c073d4202cd49
osc-jaimes <oscjaims@gmail.com>
2019-12-01 20:15:09 -0800

    Started setting up the multiplayer setup class and assosciated XML. Next step is to make multiplayer game mode playable

---
+++ app/src/main/java/com/example/pigsinapenteam2/MultiplayerSetupScreen.java
@@ -0,0 +1,14 @@
+package com.example.pigsinapenteam2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MultiplayerSetupScreen extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_multiplayer_setup_screen);
+  }
+}
