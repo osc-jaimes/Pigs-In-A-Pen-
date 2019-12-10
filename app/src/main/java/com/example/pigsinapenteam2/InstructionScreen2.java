e2b5d24d0c24ea603490715901a648be4a6ccd35
osc-jaimes <oscjaims@gmail.com>
2019-12-09 23:40:05 -0800

    added functionality for instructions screen, changes pending as we move closer to depolyment

--- app/src/main/java/com/example/pigsinapenteam2/InstructionScreen2.java
+++ app/src/main/java/com/example/pigsinapenteam2/InstructionScreen2.java
@@ -2,7 +2,9 @@ package com.example.pigsinapenteam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InstructionScreen2 extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class InstructionScreen2 extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_instruction_screen2);
+    ScreenLogic.fullScreen(this);
+  }
+
+  public void goHome(View V){
+    Intent goHome = new Intent(getApplicationContext(), MainScreen.class);
+    startActivity(goHome);
+  }
+
+  public void nextPage(View V){
+
   }
 }
--- app/src/main/java/com/example/pigsinapenteam2/InstructionsScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/InstructionsScreen.java
@@ -2,7 +2,9 @@ package com.example.pigsinapenteam2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class InstructionsScreen extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class InstructionsScreen extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_instructions_screen);
+    ScreenLogic.fullScreen(this);
+  }
+
+  public void nextPage(View V){
+    Intent goNextScreen = new Intent(getApplicationContext(), InstructionScreen2.class);
+    startActivity(goNextScreen);
+  }
+
+  public void goHome(View V){
+    Intent goHome = new Intent(getApplicationContext(), MainScreen.class);
+    startActivity(goHome);
   }
 }
