b454b7f2f49e8f6ba39f56e68cbb19974d3aa4f3
Jared <jarrrboon@gmail.com>
2019-12-14 14:40:28 -0700

    changed screens to all go fullscreen onm the ScreenLogic function rather than the long one. Also added it to screens that didn't have it

--- app/src/main/java/com/example/pigsinapenteam2/MainScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/MainScreen.java
@@ -14,13 +14,7 @@ public class MainScreen extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main_screen);
     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-    this.getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    ScreenLogic.fullScreen(this);
   }
 
   public void pressSinglePlayerButton(View v){
--- app/src/main/java/com/example/pigsinapenteam2/SinglePlayerSetupScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/SinglePlayerSetupScreen.java
@@ -20,13 +20,7 @@ public class SinglePlayerSetupScreen extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_single_player_setup_screen);
     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-    this.getWindow().getDecorView().setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    ScreenLogic.fullScreen(this);
 
 
     gameBoardSizeSpinner = findViewById(R.id.board_size_drop_down);
