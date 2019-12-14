d1bb051c6a6d3128b9a0b329c2283d69ddefe95a
Luke Rostad <rostad@ualberta.ca>
2019-12-14 14:34:30 -0700

    added class ChainMap (for storing adjacency info). Did constructors.

--- app/src/main/java/com/example/pigsinapenteam2/ChainMap.java
+++ app/src/main/java/com/example/pigsinapenteam2/ChainMap.java
@@ -1,5 +1,34 @@
 package com.example.pigsinapenteam2;
 
 public class ChainMap {
-  
+  private ChainMapCell[][] cells;
+  private int boardHeight;
+  private int boardWidth;
+
+  private final int NUMBER_OF_DIRECTIONS = 4;
+
+  public ChainMap() {
+    cells = new ChainMapCell[0][0];
+    boardHeight = 0;
+    boardWidth = 0;
+  }
+
+  public ChainMap(BoardState board) {
+    boardWidth = board.getWidth();
+    boardHeight = board.getHeight();
+    cells = new ChainMapCell[boardWidth][boardHeight];
+
+    for (int y = 0; y < boardHeight; y++) {
+      for (int x = 0; x < boardWidth; x++) {
+
+        boolean[] neighborConnected = new boolean[NUMBER_OF_DIRECTIONS];
+        for (int direction = 0; direction < NUMBER_OF_DIRECTIONS; direction++) {
+          neighborConnected[direction] = (board.getWallAi(x,y,direction) == 0);
+        }
+        ChainMapCell cell = new ChainMapCell(neighborConnected,x,y);
+        cells[x][y] = cell;
+
+      }
+    }
+  }
 }
