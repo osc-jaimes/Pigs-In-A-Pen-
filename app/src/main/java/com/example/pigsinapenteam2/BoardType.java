4cee3381e450beadfa3d11b24b638af58d7a2c90
Benjamin <bwilson1@ualberta.ca>
2019-12-14 14:04:59 -0700

    created constructor for BoardType

--- app/src/main/java/com/example/pigsinapenteam2/BoardType.java
+++ app/src/main/java/com/example/pigsinapenteam2/BoardType.java
@@ -1,4 +1,39 @@
 package com.example.pigsinapenteam2;
 
 public class BoardType {
+
+
+  private int width;
+  private int height;
+
+  //=======================================================================================
+
+  public BoardType(BoardState inputtedState, int boardID, String boardSize){
+
+    this.width = inputtedState.getWidth();
+    this.height = inputtedState.getHeight();
+
+
+    switch (boardID){
+
+      case 1:
+
+        wallsMapSetup(boardSize);
+        break;
+
+      case 2:
+
+        gardenMapSetup(boardSize);
+        break;
+
+      case 3:
+
+        hillMapSetup(boardSize);
+        break;
+
+      default:
+
+        break;
+    }//switch
+  }//constructor
 }
