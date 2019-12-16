28c1ecc73f3d4ff95d43c06a5cf14ca852eeb96c
Luke Rostad <rostad@ualberta.ca>
2019-12-16 11:01:02 -0700

    finished moving flag tests from HardBot to BoardFlags

--- app/src/main/java/com/example/pigsinapenteam2/BoardFlags.java
+++ app/src/main/java/com/example/pigsinapenteam2/BoardFlags.java
@@ -24,6 +24,40 @@ public class BoardFlags {
   }
 
   public BoardFlags(SimplifiedBoard simpleBoard) {
+    doCapturesExist = (simpleBoard.captures.size() != 0);
+    doNeutralMovesExist = (simpleBoard.neutralMoves.size() != 0);
+    doChainsExist = (simpleBoard.chains.size() != 0);
+    isEndgame = !doNeutralMovesExist;
+
+    if (doChainsExist) {
+      Chain firstChain = simpleBoard.chains.getFirst();
+      isFirstChainSizeOne = (firstChain.length == 1);
+      boolean chainHeadOpen = firstChain.getHeadOpen();
+      boolean chainTailOpen = firstChain.getTailOpen();
+      mayDoGambitHeadDirection = (chainHeadOpen && !chainTailOpen);
+      mayDoGambitTailDirection = (chainTailOpen && !chainHeadOpen);
+
+      int yourExpectedPoints = 0;
+      int theirExpectedPoints = 0;
+      for (int i = 0; i < simpleBoard.chains.size(); i++) {
+        if (i % 2 == 0) {
+          yourExpectedPoints += simpleBoard.chains.get(i).pointValue();
+        } else {
+          theirExpectedPoints += simpleBoard.chains.get(i).pointValue();
+        }
+      }
+
+      canDoGambit = (isFirstChainSizeOne &&
+          (mayDoGambitTailDirection || mayDoGambitHeadDirection));
+      shouldDoGambit = (yourExpectedPoints < theirExpectedPoints);
+
+    } else {
+      isFirstChainSizeOne = false;
+      mayDoGambitTailDirection = false;
+      mayDoGambitTailDirection = false;
+      canDoGambit = false;
+      shouldDoGambit = false;
+    }
 
   }
 }
