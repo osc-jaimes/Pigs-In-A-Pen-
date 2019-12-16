f0aaf1c7ec3549778063e517aa99bd14a8d7770f
Luke Rostad <rostad@ualberta.ca>
2019-12-16 11:10:08 -0700

    added new parent class Strategy. Children will choose move based on flags & board.

---
+++ app/src/main/java/com/example/pigsinapenteam2/Strategy.java
@@ -0,0 +1,10 @@
+package com.example.pigsinapenteam2;
+
+public class Strategy {
+  public Strategy() { }
+
+  public WallCoordinate doMove(BoardFlags flags, SimplifiedBoard board) {
+    WallCoordinate defaultMove = new WallCoordinate();
+    return defaultMove;
+  }
+}
