0bde692677c0b1d9f588f80140d3cd6d823a7afc
Benjamin <bwilson1@ualberta.ca>
2019-12-18 17:00:12 -0700

    added all small versions of maps, working on medium

--- app/src/main/java/com/example/pigsinapenteam2/SinglePlayerPlayScreen.java
+++ app/src/main/java/com/example/pigsinapenteam2/SinglePlayerPlayScreen.java
@@ -192,8 +192,68 @@ public class SinglePlayerPlayScreen extends PlayScreen {
 
       }//small gardens
       else if(SinglePlayerSetupScreen.boardType == 3){
+        ImageView hill1 = findViewById(R.id.hill1);
+        hill1.setVisibility(View.VISIBLE);
+        ImageView hill2 = findViewById(R.id.hill2);
+        hill2.setVisibility(View.VISIBLE);
 
+        Button hill1Top = findViewById(R.id.h1);
+        hill1Top.setClickable(false);
+        hill1Top.setBackgroundColor(getResources().getColor(R.color.fences));
+
+        Button hill2Top = findViewById(R.id.h4);
+        hill2Top.setClickable(false);
+        hill2Top.setBackgroundColor(getResources().getColor(R.color.fences));
+
+        Button hill3Top = findViewById(R.id.h7);
+        hill3Top.setClickable(false);
+        hill3Top.setBackgroundColor(getResources().getColor(R.color.fences));
       }//hill map small
+    }//map size small
+    else if(SinglePlayerSetupScreen.boardSize == 1){
+      if(boardType == 1){
+        return;
+      }//if statement
+
+      else if(boardType == 2){
+        ImageView garden3 = findViewById(R.id.garden3);
+        garden3.setVisibility(View.VISIBLE);
+        ImageView garden4 = findViewById(R.id.garden4);
+        garden4.setVisibility(View.VISIBLE);
+        ImageView garden5 = findViewById(R.id.garden5);
+        garden5.setVisibility(View.VISIBLE);
+        ImageView garden6 = findViewById(R.id.garden6);
+        garden6.setVisibility(View.VISIBLE);
+
+        Button garden1Top = findViewById(R.id.h12);
+        garden1Top.setClickable(false);
+        garden1Top.setBackgroundColor(getResources().getColor(R.color.fences));
+
+        Button garden1Right = findViewById(R.id.v12);
+        garden1Right.setClickable(false);
+        garden1Right.setBackgroundColor(getResources().getColor(R.color.fences));
+
+        Button garden2Top = findViewById(R.id.h16);
+        garden2Top.setClickable(false);
+        garden2Top.setBackgroundColor(getResources().getColor(R.color.fences));
+
+        Button garden2Right = findViewById(R.id.v13);
+        garden2Right.setClickable(false);
+        garden2Right.setBackgroundColor(getResources().getColor(R.color.fences));
+
+        Button garden3Top = findViewById(R.id.h17);
+        garden3Top.setClickable(false);
+        garden3Top.setBackgroundColor(getResources().getColor(R.color.fences));
+
+        Button garden3right = findViewById(R.id.v18);
+        garden3right.setClickable(false);
+        garden3right.setBackgroundColor(getResources().getColor(R.color.fences));
+
+        Button garden4Top = findViewById(R.id.h21);
+        garden4Top.setClickable(false);
+        garden4Top.setBackgroundColor(getResources().getColor(R.color.fences));
+      }
+
     }
   }
 
